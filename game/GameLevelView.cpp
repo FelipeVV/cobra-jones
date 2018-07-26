bac5d56e97f3869121ed81b435164b63b23ec48b
DESKTOP-FelipeV <felipe.vegavindas@ucr.ac.cr>
2018-07-26 10:20:47 -0600

    more comments

--- game/GameLevelView.cpp
+++ game/GameLevelView.cpp
@@ -51,30 +51,38 @@ void GameLevelView::loadLevelView(int chargeLevel)
     displayLevel();
 
 
-    // Calculate the place in the center where the player should spawn
+    /// Calculate the place in the center where the player should spawn.
+    /// First the center of the floor is measured.
+    /// Then the nearest valid point in the grid is chosen.
+    /// A valid point is a multiple of tileWidth(or tileheight).
     double spawnX = floor((screenWidth/2)/tileWidth) * tileWidth;
     double spawnY = floor((screenHeight/2)/tileHeight) * tileHeight;
-    //double spawnX = floor(tileWidth/4);
-    //double spawnY = floor(tileHeight/4);
+
+    /// debug
     qDebug() << "Width: "<< screenWidth << ". Height: " << screenHeight << NEWLINE
             << "spawnX: " << spawnX << ". spawnY: " << spawnY;
 
-	//add the player caracter
-	player = new Player(tileWidth, tileHeight, spawnX, spawnY,this);
+    /// Create player
+    player = new Player(tileWidth, tileHeight, spawnX, spawnY, this);
 	player->setSkin(1);
+
+    /// Add the player caracter
 	scene->addItem(player);
 
-    // make player focusable
+    /// Make player focusable
 	player->setFlag(QGraphicsItem::ItemIsFocusable);
 	player->setFocus();
 
-	// Set a color background
+    /// Set a color background the same color as the tile's sides.
+    /// This is because the place each tile is set in is not pixel perfect.
+    /// That creates thin black vertical lines between some tile in the level that look ugly.
+    /// A solution is to make those black lines the same color of the
 	this->view->setBackgroundBrush(QBrush(QColor(231,180,155), Qt::SolidPattern));
 
-	// The scene has infinite size, but we want it has the same size than the view
+    /// The scene has infinite size, but we want it has the same size than the view
 	this->scene->setSceneRect( this->view->rect() );
 
-	// Disable scrollbars because they are not longer needed
+    /// Disable scrollbars because they are not needed
 	this->view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 	this->view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
@@ -107,56 +115,72 @@ void GameLevelView::displayLevel()
     /// Decide tile width, taking in consideration how many must fit in the screen horizontally.
 	tileHeight = screenHeight / rows;
 
-    /// GrowthFactor is the number the dimensions of the
+    /// GrowthFactor is the number the size of the tile(imgSide) must be multiplied if to fit screen requirements(tileWidth).
 	qreal growthFactor = tileWidth / imgSide;
 
 	qDebug() << rows << "\n" << cols << "\n" << tileWidth << "\n" << tileHeight << "\n";
 
-	for(int row = 0; row < currentLevel->rows; ++row)
+    /// Traverse the entire matrix
+    for(int row = 0; row < currentLevel->rows; ++row)
 	{
 		for(int col = 0; col < currentLevel->cols; ++col)
 		{
-			qreal posX = tileWidth * static_cast<double>(col);
+            /// Get the position where tile object should spawn, based on the current row and col we are in.
+            qreal posX = tileWidth * static_cast<double>(col);
 			qreal posY = tileHeight * static_cast<double>(row);
 			qDebug() << posX << posY;
 
+            /// Create tile based on char that the matrix contains(meta data).
             Tile *currentTile = createTile(currentLevel->matrix[row][col]);
-			//change x and y
-			currentTile->setPos( QPoint(posX, posY) );
-			//transform
-			currentTile->setScale( growthFactor );
 
-            qDebug() << "tile created";
+            /// Position correctly
+			currentTile->setPos( QPoint(posX, posY) );
+
+            /// Transform, based on growth factor
+            currentTile->setScale( growthFactor );
+
+            /// Add tile so it can be shown.
 			this->scene->addItem(currentTile);
             qDebug() << "tile added to scene";
-            //tiles.append(currentTile);
 		}
     }
 }
 
+/// Remove current level
 void GameLevelView::removeLevel(int action)
 {
+    /// Create new level to change to
     Level* currentLevel=levels[chargeLevel];
+
+    /// Give new value to rows and cols (R)
     rows = static_cast<double>(currentLevel->rows);
     cols = static_cast<double>(currentLevel->cols);
+
+    /// Remove and delete player
     this->scene->removeItem(player);
     delete player;
     player=nullptr;
 
+    /// Remove every tile that was placed in the scene
     const QList<QGraphicsItem*>& allTiles = scene->items();
     for ( QGraphicsItem* item : allTiles )
     {
-        //Tile* actual = dynamic_cast<Tile*>(item);
         scene->removeItem(item);
-        //delete item;
     }
+
+    /// After current level is deleted, do something depending on the int sent. (R)
     manageAction(action);
 }
 
+/// Meaning of each action(int argument)
+/// 0: Level was lost, load current Level again.
+/// 1: Level was won, go to next level.
+/// 2: player requested to return to Menu.
 void GameLevelView::manageAction(int action){
-    if(action == 1){
+    if(action == 1)
+    {
         chargeLevel++;
-        if(chargeLevel>9)
+        if(chargeLevel>=9)
             chargeLevel=1;
         this->loadLevelView(chargeLevel);
     }
@@ -179,6 +203,6 @@ Tile* GameLevelView::createTile(QChar type)
         return new TileBroken(type);
     if(type == 'O')
         return new TileSpecial(type);
-    //else
+    /// else: create a tile with an "invalid" tile image.
     return new Tile(type);
 }
